897f5f82b67416e1e5f9e2a159c8ecb95d8e92d7
danielesantos3 <marinhodaniele957@gmail.com>
2025-02-14 22:51:11 -0300

    Criando classe pessoa

---
+++ js/ts/pessoa.js
@@ -0,0 +1,22 @@
+"use strict";
+var empresa;
+(function (empresa) {
+    class Pessoa {
+        get Nome() {
+            return this._nome;
+        }
+        set nome(nome) {
+            this._nome = nome;
+        }
+        get anoNasc() {
+            return this._anoNasc;
+        }
+        set anoNasc(anoNasc) {
+            this._anoNasc = anoNasc;
+        }
+        calcularidade(anoAtual) {
+            return anoAtual - this._anoNasc;
+        }
+    }
+    empresa.Pessoa = Pessoa;
+})(empresa || (empresa = {}));
